476cfc94c287ddd941e445d9abad679c507b7f76
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-05-08 16:56:55 +0100

    install administrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_08_155109) do
+ActiveRecord::Schema.define(version: 2021_05_08_155359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2021_05_08_155109) do
     t.integer "customer_id"
     t.datetime "purchased_at"
     t.boolean "processed_order"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "category"
+    t.integer "weight"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
